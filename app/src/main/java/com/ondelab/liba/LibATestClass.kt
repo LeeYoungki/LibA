a1ae92230c15ab795808f1912f75265d651418d9
YK_OnDeLab_PC <carydev@ondelab.com>
2021-02-23 13:47:53 +0900

    LibA Test

---
+++ app/src/main/java/com/ondelab/liba/LibATestClass.kt
@@ -0,0 +1,12 @@
+package com.ondelab.liba
+
+import android.util.Log
+import com.ondelab.testlibs.TestClass
+
+class LibATestClass {
+    fun foo(msg:String){
+        Log.e("LibATestClass", "foo: $msg 그리고 호출한다 라이브러리B" )
+        val test = TestClass()
+        test.foo(msg)
+    }
+}
