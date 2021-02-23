4a0e41ca57a6bc62bf45d9549fe2242a6452ec07
YK_OnDeLab_PC <carydev@ondelab.com>
2021-02-23 14:49:24 +0900

    되돌리기

--- app/src/main/java/com/ondelab/liba/LibATestClass.kt
+++ app/src/main/java/com/ondelab/liba/LibATestClass.kt
@@ -4,9 +4,9 @@ import android.util.Log
 //import com.ondelab.testlibs.TestClass
 
 class LibATestClass {
-    fun foo(msg:String){
-        Log.e("LibATestClass", "foo: $msg 그리고 호출한다 라이브러리B" )
+//    fun foo(msg:String){
+//        Log.e("LibATestClass", "foo: $msg 그리고 호출한다 라이브러리B" )
 //        val test = TestClass()
 //        test.foo(msg)
-    }
+//    }
 }
